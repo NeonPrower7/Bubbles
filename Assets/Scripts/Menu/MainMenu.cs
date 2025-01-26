a832127bf229b2f0c82d9c60e7cf222530b0f45e
NeonPrower7 <shchenova.katerina@gmail.com>
2025-01-26 14:06:28 +0200

    Buildable

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void OptionsHandler()
+    {
+
+    }
+
     public void ExitHandler()
     {
         Application.Quit();
